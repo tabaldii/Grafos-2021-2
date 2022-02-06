5ea2eb7285f025f9c97cc79d80163941122c4a33
Anderson H. G. Tabaldi <tabaldi33@gmail.com>
2022-02-05 23:00:35 -0300

    funcionando

--- BFS.c
+++ BFS.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 6
+#define TAM 8
 
 struct dado {
     int elemento;
@@ -50,44 +50,44 @@ Fila removeEle(Fila fila) {
 void showFila(Fila fila) {
 
     Dado *aux;
-    printf("Fila -> ");
-    for (aux = fila.primeiro; aux != NULL; aux = aux->next){
+    printf("\nFila -> ");
+    for (aux = fila.primeiro; aux != NULL; aux = aux->next) {
         printf("%d, ", aux->elemento);
     }
     printf("\n");
-    printf("-------\n");
+    printf("-------------\n\n\n");
 }
 
 void BFS(int inicio, int matriz[TAM][TAM], Fila fila,  int anterior[TAM], int visitado[TAM], int distancia[TAM]) {
 
     int i = inicio, j;
     fila = incluiEle(fila, inicio);
+    visitado[i] = 1;
 
-    while (visitado[i] != 1) {
+    while (fila.primeiro != NULL) {
 
-        visitado[i] = 1;
-
+        fila = removeEle(fila);
+        printf("--------------\n");
         printf("Visitando -> %d\n", i);
-        printf("------------\n");
-        printf("     vertice    |    visitado  |     valor de caminho    |   anterior    |\n");
+        printf("--------------\n");
+        printf("     vertice    |    visitado   | custo caminho |   anterior    |\n");
 
         for(int k = 0; k < TAM; k++) {
             printf("\t%d\t|\t%d\t|\t%d\t|\t%d\t|", k, visitado[k], distancia[k], anterior[k]);
             printf("\n");
         }
-        showFila(fila);
 
         for (j = 0; j < TAM; j++) {
-                if (matriz[i][j] != 0) {
-                    if (visitado[j] != 1) {
-                        fila = incluiEle(fila, j);
-                    }
+            if (matriz[i][j] == 1) {
+                if (visitado[j] != 1) {
+                    visitado[j] = 1;
+                    fila = incluiEle(fila, j);
                     distancia[j] = distancia[i] + 1;
                     anterior[j] = i;
                 }
+            }
         }
-
-        fila = removeEle(fila);
+        showFila(fila);
         i = fila.primeiro->elemento;
     }
 }
@@ -104,13 +104,15 @@ int main() {
 
 
     int matriz [TAM][TAM] = {
-            //0  1  2  3  4  5
-    /*0*/   { 0, 1, 0, 1, 0, 0},
-    /*1*/   { 1, 0, 1, 0, 0, 0},
-    /*2*/   { 0, 1, 0, 0, 0, 0},
-    /*3*/   { 1, 0, 0, 0, 1, 1},
-    /*4*/   { 0, 0, 0, 1, 0, 1},
-    /*5*/   { 0, 0, 0, 1, 1, 0},
+            //0  1  2  3  4  5  6  7
+    /*0*/   { 0, 0, 1, 1, 0, 0, 0, 0},
+    /*1*/   { 1, 0, 0, 1, 0, 0, 1, 0},
+    /*2*/   { 0, 0, 0, 0, 0, 1, 0, 0},
+    /*3*/   { 0, 0, 0, 0, 0, 1, 0, 0},
+    /*4*/   { 0, 0, 0, 0, 0, 0, 0, 0},
+    /*5*/   { 0, 0, 0, 0, 0, 0, 0, 0},
+    /*6*/   { 0, 0, 0, 0, 0, 1, 0, 0},
+    /*7*/   { 0, 0, 0, 0, 0, 1, 0, 0},
     };
 
 
@@ -119,7 +121,7 @@ int main() {
     for (int i = 0; i < TAM; i++) {
         anterior[i] = -1;
         visitado[i] = 0;
-        distancia[i] = NULL;
+        distancia[i] = 0;
     }
     
     Fila fila;
@@ -131,97 +133,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
-/*
-struct elemFila {
-tipoElemFila info;
-struct elemFila * prox;
-};
-typedef struct elemFila ElemFila;
-struct fila {
-ElemFila * ini;
-ElemFila * fim;
-};
-Fila * fila_cria(void)
-{
-Fila * f = (Fila *)malloc(sizeof(Fila));
-f->ini = f->fim = NULL;
-return f;
-}
-int fila_vazia(Fila * f)
-{
-return (f->ini == NULL);
-}
-void fila_insere(Fila * f, tipoElemFila v)
-{
-ElemFila * n = (ElemFila *)malloc(sizeof(ElemFila));
-assert(!(n == NULL)); // ou: if (n == NULL) { printf("sem memoria\n"); exit(1); }
-n->info = v; // armazena informacao
-n->prox = NULL; // novo no' passa a ser o ultimo
-if (!fila_vazia(f)) // verifica se lista nao e’ vazia
-f->fim->prox = n;
-else // fila estava vazia
-f->ini = n;
-f->fim = n; // fila aponta para novo elemento
-}
-tipoElemFila fila_retira(Fila * f)
-{
-ElemFila * t;
-tipoElemFila v;
-assert(!fila_vazia(f)); // ou: if (fila_vazia(f)) {printf("Fila vazia.\n"); exit(1);}
-t = f->ini;
-v = t->info;
-f->ini = t->prox;
-if (f->ini == NULL) // verifica se fila ficou vazia
-f->fim = NULL;
-free(t);
-return v;
-}
-void fila_libera(Fila * f)
-{
-ElemFila * q = f->ini;
-ElemFila * t;
-while (q != NULL)
-{
-t = q->prox;
-free(q);
-q = t;
-}
-free(f);
-}
-void fila_imprime(Fila * f)
-{
-ElemFila * p;
-printf("Fila:\n");
-if (fila_vazia(f))
- return;
-for (p = f->ini; p != NULL; p = p->prox)
-printf("%f ", p->info);
-printf("\n");
-}
-*/
-
-
-/*
-1 for cada vértice u  V[G] – {s}
-2 do cor[u] <- BRANCO
-3 d[u] <- 
-4 pai[u] <- NULL
-5 cor[s] <- CINZA
-6 d[s] <- 0
-7 pai[s] <- NULL
-8 Q <- 
-9 ENQUEUE(Q, s) //Insere s na fila Q
-10 while Q 
-11 do u <- DEQUEUE(Q) //Consulta e remove o primeiro elemento da fila Q
-12 for cada v <- Adj[u]
-13 do if cor[v] = BRANCO
-14 then cor[v] <-CINZA
-15 d[v] <- d[u] + 1
-16 pai[v] <- u
-17 ENQUEUE (Q, v)
-18 cor[u] <- PRETO
-*/
